dd53329d429b394c27e472fd36f57219f77a1f2f
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-24 22:18:46 +0100

    Removed nesting under /users

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,7 +1,6 @@
 class ProvidersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index]
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
     @providers = Provider.all
   end
-
 end
