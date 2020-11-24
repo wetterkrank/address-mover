fcd4623588cdca4487a22cfa63a98a3c6e26fff6
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-24 08:51:32 +0100

    Generated the Address Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_164528) do
+ActiveRecord::Schema.define(version: 2020_11_24_074730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "city"
+    t.string "street_number"
+    t.string "street_name"
+    t.string "zip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
