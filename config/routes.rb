3313b84d70000f72f8948f31cdb4473bdc5222bd
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-24 14:57:54 +0100

    Created the my providers backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  resources :user do
+    resources :my_providers, only: [ :index, :show, :new, :create, :destroy ]
+  end
   resources :addresses, only: [ :index, :show ]
 end
