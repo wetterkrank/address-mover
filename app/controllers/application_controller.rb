5efc626ae2039d917d1d7476504098d69ffcd6f0
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-24 11:19:51 +0100

    User views checked and updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :birthday, :planned_move_date])
+  end
 end
