270dd4a5109d9c9d2507d5c775271363c5704ad5
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-25 12:26:11 +0100

    Created the pundit for my_providers

--- app/controllers/my_providers_controller.rb
+++ app/controllers/my_providers_controller.rb
@@ -1,17 +1,30 @@
 class MyProvidersController < ApplicationController
 
     def index 
+      @my_providers = policy_scope(MyProvider).order(created_at: :desc)
     end
 
     def show
+      authorize @my_provider
+    end
+
+    def edit 
+      authorize @my_provider
     end
 
     def new 
+      authorize @my_provider
+    end
+
+    def update
+      authorize @my_provider
     end
 
     def create
+      authorize @my_provider
     end
 
     def destroy
+      authorize @my_provider
     end
 end
