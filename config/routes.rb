5e1305600b192afacc8c8070f1088b0a29ff413e
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-11-26 12:45:50 +0100

    created about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
 
   resources :users
   resources :providers, only: [ :index, :show, :edit, :update, :destroy ]
   resources :my_providers, only: [ :index, :show, :new, :create, :destroy ]
-  
+
   resources :moves do
     resources :updates, only: [ :index, :edit ]
   end
