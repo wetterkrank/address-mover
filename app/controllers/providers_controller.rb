8682ad69a702d06cf4b75fcdfa3afbac524f58f3
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-28 17:31:44 +0100

    Selection controller refactor + CSS feedback for providers list

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -5,8 +5,6 @@ class ProvidersController < ApplicationController
     skip_policy_scope
     @providers = Provider.all
     @selected_ids = current_user.my_providers.pluck(:provider_id)
-    @icons = { plus: ActionController::Base.helpers.image_url("icons/plus.svg"),
-               check: ActionController::Base.helpers.image_url("icons/checked.svg") }
   end
 
   def show
